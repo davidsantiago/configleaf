cfe29a9cd5f1d6fcaef1d59d2fe6ff655486ad8a
David Santiago <david.santiago@gmail.com>
2011-08-02 02:24:49 -0500

    Add .configleaf/ to .gitignore.

--- project.clj
+++ project.clj
@@ -1,4 +1,12 @@
 (defproject configleaf "0.1.0-SNAPSHOT"
   :description "Software configuration for Clojure"
-  :dependencies [[org.clojure/clojure "1.2.0"]]
-  :dev-dependencies [[ritz "0.1.7"]])
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [stencil "0.1.2"]
+                 [robert/hooke "1.1.2"]]
+  :dev-dependencies [[ritz "0.1.7"]]
+
+  ;  :eval-in-leiningen true
+  :hooks [leiningen.configleaf]
+
+  :configleaf {:configurations {:test {}}
+               :namespace test.testing})
--- src/configleaf/core.clj
+++ src/configleaf/core.clj
@@ -1,2 +1,25 @@
-(ns configleaf.core)
+(ns configleaf.core
+  (:require [clojure.java.io :as io]))
+
+(defn read-current-config
+  "Read the current configuration from the config file for the project
+   at the given path. For example, if the argument is \"/home/david\", reads
+   \"/home/david/.configleaf/current\". .configleaf/current should contain a
+   single form that names the current configuration."
+  [config-store-path]
+  (let [config-file (io/file config-store-path ".configleaf/current")]
+    (when (and (.exists config-file)
+               (.isFile config-file))
+      (read-string (slurp config-file)))))
+
+(defn save-current-config
+  "Store the given configuration as the new current config in for the project
+   at the given path."
+  [config-store-path current-config]
+  (let [config-dir (io/file config-store-path ".configleaf")
+        config-file (io/file config-store-path ".configleaf/current")]
+    (if (not (and (.exists config-dir)
+                  (.isDirectory config-dir)))
+      (.mkdir config-dir))
+    (spit config-file (prn-str current-config))))
 
