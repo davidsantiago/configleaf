fee50a9faf48f4739aa9dc3e46eac9140df67818
David Santiago <david.santiago@gmail.com>
2011-08-15 20:54:24 -0500

    Change version to 0.2.0-SNAPSHOT for next iteration.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject configleaf "0.1.0"
+(defproject configleaf "0.2.0-SNAPSHOT"
   :description "Software configuration for Clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [stencil "0.1.2"]
