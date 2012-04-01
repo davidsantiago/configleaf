17de8645e801b2465c21efa95e626f5686493b5d
David Santiago <david.santiago@gmail.com>
2012-04-01 13:39:19 -0700

    Remove :dev-dependencies key from project.clj

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [stencil "0.1.2"]
                  [robert/hooke "1.1.2"]]
-  :dev-dependencies [[ritz "0.1.7"]]
 
   :eval-in-leiningen true
   :hooks [configleaf.hooks]
