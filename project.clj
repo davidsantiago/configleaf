0e078e017b5b2da492e7bca3aad27a24ef3727f3
David Santiago <david.santiago@gmail.com>
2012-04-04 14:28:11 -0700

    Remove :hooks key in project.clj; was causing hooks to run when we built release jars.

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject configleaf "0.4.3"
+(defproject configleaf "0.4.4-SNAPSHOT"
   :description "Persistent and buildable profiles in Leiningen."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [stencil "0.1.2"]
                  [robert/hooke "1.1.2"]]
 
   :eval-in-leiningen true
-  :hooks [configleaf.hooks]
 
   :profiles {:test {:params {:test 1}
                     :java-properties {"test" "1"
