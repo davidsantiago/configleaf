3066c1a6616d79bdc721db44ca3d1c77cdc1e27d
David Santiago <david.santiago@gmail.com>
2012-04-04 01:31:08 -0700

    Update hooks to also add a dependency for configleaf itself to the project.

--- src/configleaf/hooks.clj
+++ src/configleaf/hooks.clj
@@ -23,13 +23,25 @@
       (println "Configleaf warning: Multiple leiningen-core jars found."))
     lein-core-version))
 
-(defn add-lein-core-dep
-  "Given a project map, inserts a dependency for leiningen-core."
+(defn find-configleaf-in-project
+  "Given a project map, find the configleaf entry in the :plugins key, so we can
+   figure out what version we are."
   [project]
-  (let [lein-core-version (find-lein-core-on-classpath)]
+  (last (filter #(or (= (first %) 'configleaf)
+                     (= (first %) 'configleaf/configleaf))
+                (:plugins project))))
+
+(defn add-self-deps
+  "Given a project map, inserts a dependency for leiningen-core and configleaf
+   itself, so that tasks that run in the project can still run these hooks."
+  [project]
+  (let [lein-core-version (find-lein-core-on-classpath)
+        configleaf-dep-entry (find-configleaf-in-project project)]
     (project/merge-profile project
                            {:dependencies [['leiningen-core
-                                            lein-core-version]]})))
+                                            lein-core-version]
+                                           ['configleaf
+                                            (second configleaf-dep-entry)]]})))
 
 ;;
 ;; Leiningen hooks
@@ -39,7 +51,7 @@
   "A hook to set up the namespace with the configuration before running a task.
    Only meant to hook leiningen.core.main/apply-task."
   [task & [task-name project & args]]
-  (let [configured-project (merge-profiles (add-lein-core-dep project)
+  (let [configured-project (merge-profiles (add-self-deps project)
                                            (get-current-profiles))]
     (output-config-namespace configured-project)
     (if (get-in configured-project [:configleaf :verbose])
@@ -52,7 +64,7 @@
    in the project. Works by adding a call to require-config-namespace to the
    init arg."
   [task & [project form init]]
-  (let [configured-project (merge-profiles (add-lein-core-dep project)
+  (let [configured-project (merge-profiles (add-self-deps project)
                                            (get-current-profiles))]
     (task configured-project form
           `(do (require 'configleaf.core)
