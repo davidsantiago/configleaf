38f6733a2f63c6dc9122d145221b0f7759c38c69
David Santiago <david.santiago@gmail.com>
2012-07-24 02:24:59 -0700

    Implement :never-sticky configuration key.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject configleaf "0.4.5-SNAPSHOT"
+(defproject configleaf "0.4.6-SNAPSHOT"
   :description "Persistent and buildable profiles in Leiningen."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [stencil "0.3.0"]
@@ -6,6 +6,12 @@
 
   :eval-in-leiningen true
 
+  :hooks [configleaf.hooks]
   :profiles {:test {:params {:test 1}
                     :java-properties {"test" "1"
-                                      :test2 "2"}}})
+                                      :test2 "2"}}
+             :a {}
+             :b {}
+             :c {}
+             :d [:a :b]}
+  :configleaf {:never-sticky [:d]})
--- test/configleaf/test/core.clj
+++ test/configleaf/test/core.clj
@@ -1,5 +1,28 @@
 (ns configleaf.test.core
-  (:use [configleaf.core] :reload)
+  (:use configleaf.core)
   (:use [clojure.test]))
 
+(deftest expand-profile-test
+  (is (= [:a] (expand-profile {:profiles {:a {} :b {}}} :a)))
+  (is (= [:a] (expand-profile {:profiles {:a {} :b [:a]}} :b)))
+  (is (= [:b] (expand-profile {:profiles {:a {} :b [:a] :c [:b]}} :c))))
+
+(deftest fully-expand-profile-test
+  (is (= #{:a} (fully-expand-profile {:profiles {:a {} :b {}}} :a)))
+  (is (= #{:a} (fully-expand-profile {:profiles {:a {} :b [:a]}} :b)))
+  (is (= #{:a} (fully-expand-profile {:profiles {:a {} :b [:a] :c [:b]}} :c)))
+  (is (= #{:a :b} (fully-expand-profile {:profiles {:a {} :b {} :c [:a] :d [:b]
+                                                    :e [:c :d]}} :e)))
+  (is (= #{:a :b} (fully-expand-profile {:profiles {:a {} :b {} :c [:a]
+                                                    :d [:c :b]}}
+                                        :d))))
+
+(deftest unstickable-profiles-test
+  (is (= #{#{:a :b}}
+         (unstickable-profiles {:profiles {:a {} :b {} :c {} :d [:a :b]}
+                                :configleaf {:never-sticky [:d]}})))
+  (is (= #{#{:a :c} #{:b :d}}
+         (unstickable-profiles {:profiles {:a {} :b {} :c {} :d {}
+                                           :ac [:a :c] :bd [:b :d]}
+                                :configleaf {:never-sticky [:ac :bd]}}))))
 
